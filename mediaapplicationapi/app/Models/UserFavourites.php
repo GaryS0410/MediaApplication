79400a1bf874047a08b21ce4c9b2fa3b743082e0
GaryS0410 <garyjsmith0410@gmail.com>
2026-01-15 20:50:41 +0000

    Updated .gitignore, deleted models, and some work on user model to define inital attributes and define one-to-many relationship with User table

--- mediaapplicationapi/app/Models/UserFavourites.php
+++ mediaapplicationapi/app/Models/UserFavourites.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/** 
+ *  @property int $id
+ *  @property string $api_lookup_id
+ *  @property int $user_id
+ */
 class UserFavourites extends Model
 {
-    //
+    protected $fillable = ['api_lookup_id', 'user_id'];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
